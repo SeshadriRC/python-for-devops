8c9d25e39f785b3aa42de1a15adf1423c81bcd43
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-04-20 22:09:39 +0530

    Update hello-world.py

--- Day-15/examples/hello-world.py
+++ Day-15/examples/hello-world.py
@@ -5,5 +5,7 @@ app = Flask(__name__)  # Its a common line, every flask program will have this. 
 @app.route('/')        # A decorator is a shortcut that adds extra functionality to a function using @ syntax. It tells Flask: when someone visits /, run the hello_world() function.
 def hello_world():
     return 'Hello, World!'
-    
-    app.run("0.0.0.0")     # Run on the local machine ip address, by default flask application will run on port 5000
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=8080)     # Run on the local machine ip address, by default flask application will run on port 5000
+  
